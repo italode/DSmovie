841588692a1596d25fbfb1bcfd93eeeefbfcbe39
Italo Pereira <italo.deliba@hotmail.com>
2022-01-13 16:40:23 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+import { ReactComponent as Githubicon } from 'assets/img/github.svg';
+import './style.css';
+
+function Navbar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a href="https://github.com/italode">
+            <div className="dsmovie-contact-container">
+              <Githubicon />
+              <p>/devsuperior</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+
+}
+
+export default Navbar;
