8f779bfbaefa98c785838cf129285f45b1309a6a
Italo Pereira <italo.deliba@hotmail.com>
2022-01-14 16:24:39 -0300

    MovieCard

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,10 +1,32 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/pagination";
 
 function listing() {
 
   return (
-    <Pagination />
-  )
+    <>
+      <Pagination />
+
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg- 4mb-3">
+            <MovieCard />
+          </div><div className="col-sm-6 col-lg-4 mb-3">
+            <MovieCard />
+          </div><div className="col-sm-6 col-lg-4 mb-3">
+            <MovieCard />
+          </div><div className="col-sm-6 col-lg-4 mb-3">
+            <MovieCard />
+          </div>
+        </div>
+
+      </div>
+
+    </>
+  );
 }
 
 export default listing; 
